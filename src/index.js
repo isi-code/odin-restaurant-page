8310c40ff562f1afbb3f24594dc457aba8290281
isi-code <irvinjose.silva@gmail.com>
2025-12-24 16:33:56 -0800

    Make classes follow SOLID princples

--- src/index.js
+++ src/index.js
@@ -1,35 +1,25 @@
-function PageCreator(pageContainer, title, ...pageSections) {
+function PageContentGenerator(title, ...pageSections) {
     if (!new.target){
         throw Error("new keyword wasn't used");
     }
-
-    this.pageContentContainer = document.getElementById(pageContainer);
-    this.removePageContent();
-    this.addPageContent(this.generatePageContent(title, pageSections));
+    this.pageTitle = title;
+    this.pageSections = pageSections;
 }
-
-PageCreator.prototype.createPageTitleSection = function (pageTitle) {
+PageContentGenerator.prototype.createPageTitleSection = function () {
         const titleSection = document.createElement("section");
         titleSection.setAttribute("id","title-section");
         const titleTag = document.createElement("h1");
         
-        titleTag.textContent = pageTitle;
+        titleTag.textContent = this.pageTitle;
         titleSection.append(titleTag);
 
         return titleSection
 }
-
-PageCreator.prototype.removePageContent = function () { 
-    while (this.pageContentContainer.lastChild) {
-        this.pageContentContainer.lastChild.remove();
-    }
-}
-
-PageCreator.prototype.generatePageContent = function (title, pageSections) {
+PageContentGenerator.prototype.generatePageContent = function () {
     const pageContent = [];
-    pageContent.push(this.createPageTitleSection(title));
+    pageContent.push(this.createPageTitleSection());
 
-    pageSections.forEach(pageSectionContent => {
+    this.pageSections.forEach(pageSectionContent => {
         const pageSection = document.createElement("section");
         pageSectionContent.forEach( (content) => {
             pageSection.innerHTML += content;
@@ -40,11 +30,25 @@ PageCreator.prototype.generatePageContent = function (title, pageSections) {
     return pageContent
 }
 
-PageCreator.prototype.addPageContent = function (pageContent) { this.pageContentContainer.append(...pageContent); }
-
+function PageContentRenderer(pageContainer){
+    if (!new.target){
+        throw Error("new keyword wasn't used");
+    }
+    this.pageContentContainer = document.getElementById(pageContainer);
+    this.removePageContent();
+}
+PageContentRenderer.prototype.removePageContent = function () { 
+    while (this.pageContentContainer.lastChild) {
+        this.pageContentContainer.lastChild.remove();
+    }
+}
+PageContentRenderer.prototype.renderPageContent = function (pageContent) { this.pageContentContainer.append(...pageContent); }
 
 const homepageContent = [ 
     ['<h3>About Us</h3>','<p>Greek Cuisine Paradise is an authentic, family-owned escape that brings the vibrant flavors and sun-drenched atmosphere of the Mediterranean right to your table. Specializing in traditional Hellenic recipes passed down through generations, the restaurant offers a menu rich in fresh herbs, premium olive oils, and flame-grilled meat</p>'],
     ['<h3>Hours</h3>','<h4>Sunday</h4>','CLOSED','<h4>Monday</h4>','9AM - 8PM','<h4>Tuesday</h4>','7AM - 8PM','<h4>Wednesday</h4>','7AM - 8PM','<h4>Thursday</h4>','7AM - 8PM','<h4>Friday</h4>','7AM - 8PM','<h4>Saturday</h4>','7AM - 2PM']
 ];
-const home = new PageCreator("content", "Greek Cuisine Paradise", ...homepageContent);
+
+const homePage = new PageContentGenerator("Greek Cuisine Paradise", homepageContent);
+const pageRenderer = new PageContentRenderer("content");
+pageRenderer.renderPageContent(homePage.generatePageContent());
